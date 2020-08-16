274c929d7ed4dfa12e8482ecad94361545038b0b
vagrant <akzouksia@icloud.com>
2020-08-16 06:45:20 +0000

    [Finish]2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
   before_action :ensure_correct_user, only: [:update]
+
+  
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
 
   def show
     @user = User.find(params[:id])
@@ -7,19 +17,20 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
-
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render 'edit'
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path, notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
