1ca0b4389195cdb02d1936b82bc82ff1ac6fb0fc
vagrant <akzouksia@icloud.com>
2020-08-19 04:17:33 +0000

    [Finish]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,15 +5,13 @@ class BookCommentsController < ApplicationController
 		book = Book.find(params[:book_id])
 	    @book_comment = current_user.book_comments.new(book_comment_params)
 	    @book_comment.book_id = book.id
-    if  @book_comment.save
+		@book_comment.save
 	    flash[:notice] = "You have created book_comment successfully."
-	end
-		redirect_to request.referer
 	end
 
 	def destroy
+		@book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
 		BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    	redirect_to request.referer
 	end
 
 	private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
   	book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-   
+    @book = favorite.book
   end
 
   def destroy
   	book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    
+    @book = favorite.book
   end
 end
