c5369f14666eac0b81aa6aab52e01706a79220f6
vagrant <akzouksia@icloud.com>
2020-08-19 04:48:48 +0000

    [Finish]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,11 +7,14 @@ class BookCommentsController < ApplicationController
 	    @book_comment.book_id = book.id
 		@book_comment.save
 	    flash[:notice] = "You have created book_comment successfully."
+	    @book = Book.find(params[:book_id])
 	end
 
 	def destroy
 		@book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+		@book = @book_comment.book
 		BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+
 	end
 
 	private
